8d6c8bd9d56adb5bbe20c1904de6203846e1e924
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2018-02-25 23:13:32 +0100

    Add board display

--- src/datatypes.nim
+++ src/datatypes.nim
@@ -2,9 +2,10 @@
 # Distributed under the Apache v2 License
 # (license terms are at https://www.apache.org/licenses/LICENSE-2.0).
 
+import tables
 
 type
-  Intersection* = enum
+  Stone* = enum
     Empty, Black, White, Border # TODO, will having Empty = 0 or Black = 0 + White = 1 faster?
     # If memory/cache speed is a bottleneck, consider packing
 
@@ -20,7 +21,7 @@ type
     else:
       discard
 
-  Board*[N: static[int]] = array[(N + 2) * (N + 2), Intersection]
+  Board*[N: static[int]] = array[(N + 2) * (N + 2), Stone]
     # To ease boarder detection in algorithms, boarders are also represented as an (invalid) intersection
 
   BoardState*[N: static[int]] = object
@@ -42,6 +43,38 @@ type
     komi: float32
     # ruleset
 
+proc initBoard(size: static[int]): Board[size] {.noInit, noSideEffect.} =
+
+  for i, mstone in result.mpairs:
+    # Set borders
+    if  i < size+2 or             # first row
+        i >= (size+1)*(size+2) or # last row
+        i mod (size+2) == 0 or    # first column
+        i mod (size+2) == size+1: # last column
+      mstone = Border
+    else:
+      mstone = Empty
+
+
+const stone_display = {
+  Empty: '.',
+  Black: 'X',
+  White: 'O',
+  Border: ' '}.toTable
+
+proc `$`*[N: static[int]](board: Board[N]): string =
+  # Display a go board
+
+  # The go board as an extra border on the top, left, right and bottom
+  # So a 19x19 board is actually 21x21 and the end of line is at the 20th position of each line
+
+  result = ""
+
+  for i, stone in board:
+    result.add stone_display[stone]
+    if i mod (N+2) == N+1: # Test if we reach end of line
+      result.add '\n'
+
 when isMainModule:
 
   let a = BoardState[19](next_player: Black)
@@ -53,5 +86,7 @@ when isMainModule:
   echo "Size of nb_stones: " & $sizeof(a.nb_stones)
   echo "Size of last move: " & $sizeof(a.last_move)
   echo "Size of Move: " & $sizeof(Move)
-  echo "Size of Intersection: " & $sizeof(Intersection)
+  echo "Size of Intersection: " & $sizeof(Stone)
+
+  echo initBoard(9)
 
